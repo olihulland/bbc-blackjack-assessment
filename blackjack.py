b0ae18c85a6bc03adc2ff8eb84f31e174c70fbbe
Oliver Hulland <olihulland@gmail.com>
2022-12-29 21:11:07 +0000

    Include provided starter code and brief
    Uploaded the starter code provided in the candidate pack and included the candidate instruction brief doc

---
+++ blackjack.py
@@ -0,0 +1,9 @@
+from src.deck import Deck
+
+
+def play():
+    print('Hello, potential future BBC developer!')  # execution starts here! delete this line and add your game code.
+
+
+if __name__ == '__main__':
+    play()
