221d8c2a8b05843b83b289507d7ec64520955cf3
Oliver Hulland <olihulland@gmail.com>
2023-01-05 16:01:17 +0000

    Added calculate hand total
    Calculate total score of hand considering aces and included unit tests

--- src/hand.py
+++ src/hand.py
@@ -13,3 +13,33 @@ class Hand:
 
     def __str__(self):
         return str(self.cards)
+
+    def getBestTotal(self) -> int:
+        """Get the best total of the hand.
+        
+        Returns:
+            int: The best total of the hand.
+        """
+        total = 0
+        numOfAces = 0
+
+        for card in self.cards:
+            value = card.getValue()
+            total += value[0]
+
+            if len(value) > 1:   # if card has choices then is an ace
+                numOfAces += 1
+        
+        while numOfAces > 0 and total <= 21-10:  # if have room for big ace then add one
+            total += 10
+            numOfAces -= 1
+        
+        return total
+
+    def checkBust(self) -> bool:
+        """Check if the hand is bust.
+        
+        Returns:
+            bool: True if the hand is bust, False otherwise.
+        """
+        return self.getBestTotal() > 21
--- test/test_hand.py
+++ test/test_hand.py
@@ -15,6 +15,21 @@ class HandTestCase(unittest.TestCase):
         number_of_cards = len(self.hand.cards)
         self.assertEqual(number_of_cards, 2)
 
+    def test_score_hand_1(self):
+        testHand = Hand(self.deck)
+        testHand.cards = [Card("Hearts", "K"), Card("Hearts", "A")]
+        self.assertEqual(testHand.getBestTotal(), 21)
+
+    def test_score_hand_2(self):
+        testHand = Hand(self.deck)
+        testHand.cards = [Card("Hearts", "K"), Card("Hearts", "Q"), Card("Hearts", "A")]
+        self.assertEqual(testHand.getBestTotal(), 21)
+
+    def test_score_hand_3(self):
+        testHand = Hand(self.deck)
+        testHand.cards = [Card("Hearts", "9"), Card("Hearts", "A"), Card("Hearts", "A")]
+        self.assertEqual(testHand.getBestTotal(), 21)
+
 
 if __name__ == '__main__':
     unittest.main()
